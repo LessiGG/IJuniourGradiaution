d039f29aaa9f56f031044e627d4e61635b48efb5
LessiGG <ivan-lesnichenko@mail.ru>
2023-01-25 19:06:33 +0300

    Removed button`s unity dependences

---
+++ Assets/Scripts/FinishGameWindow.cs
@@ -0,0 +1,39 @@
+﻿using Coins;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class FinishGameWindow : MonoBehaviour
+{
+    [SerializeField] private CoinManager _coinManager;
+    
+    private Button _nextLevelButton;
+
+    private void OnEnable()
+    {
+        _nextLevelButton = GetComponent<Button>();
+        _nextLevelButton.onClick.AddListener(OnButtonClick);
+    }
+
+    private void OnDestroy()
+    {
+        _nextLevelButton.onClick.RemoveListener(OnButtonClick);
+    }
+
+    private void OnButtonClick()
+    {
+        MoveToNextLevel();
+    }
+
+    private void MoveToNextLevel()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            _coinManager.SaveProgress();
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    } 
+}
---
+++ Assets/Scripts/GameStartWindow.cs
@@ -0,0 +1,40 @@
+﻿using Player.Behaviours;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class GameStartWindow : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _levelText;
+    
+    private Button _playButton;
+
+    private void OnEnable()
+    {
+        _playButton = GetComponent<Button>();
+        _playButton.onClick.AddListener(OnButtonClick);
+    }
+
+    private void OnDisable()
+    {
+        _playButton.onClick.RemoveListener(OnButtonClick);
+    }
+
+    private void OnButtonClick()
+    {
+        Play();
+    }
+
+    private void Start()
+    {
+        _levelText.text = SceneManager.GetActiveScene().name;
+    }
+
+    private void Play()
+    {
+        FindObjectOfType<WindowManager>().HideStartWindow();
+        FindObjectOfType<PlayerBehaviour>().EnableInput();
+    }
+}
